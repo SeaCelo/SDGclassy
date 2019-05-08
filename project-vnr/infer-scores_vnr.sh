0f1118e1c96782b7e2aef431d6ceff410216cb46
SeaCelo <SeaCelo@users.noreply.github.com>
2019-05-08 15:42:34 -0400

    update to new version

--- project-vnr/infer-scores_vnr.sh
+++ project-vnr/infer-scores_vnr.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+set -e          #needed to ignore errors?
 
 d='~/Documents/GitHub/SDGclassy/' #specify root path
-p='project-vnr'                  #project subdirectory
+p='project-wess'                  #project subdirectory
 c='cl_base'                       #select classifier name
-f='vnr_test'                      #select target files
+f='wess_txt'                      #select target files
 
-cd "${d}" && \
-rm "${d}"/"${p}"/"${f}"/.DS_Store ; \
-mallet import-dir --input "${d}"/"${p}"/"${f}"  --output "${d}"/"${p}"/workshop/inferring-"${f}".mallet --keep-sequence --remove-stopwords --extra-stopwords "${d}"/workshop/extra-exclude-words.txt --keep-sequence-bigrams --gram-sizes 1,2  --use-pipe-from "${d}"/classifier/"${c}".mallet   && \
-mallet infer-topics --input "${d}"/"${p}"/workshop/inferring-"${f}".mallet --inferencer "${d}"/classifier/"${c}"-inferencer.mallet --output-doc-topics "${d}"/"${p}"/output/scores-"${f}"-"${c}".txt   && \
+cd "${d}" 
+rm "${d}"/"${p}"/"${f}"/.DS_Store || true   #we ignore any errors here
+
+mallet import-dir --input "${d}"/"${p}"/"${f}"  --output "${d}"/"${p}"/workshop/inferring-"${f}".mallet --keep-sequence --remove-stopwords --extra-stopwords "${d}"/workshop/extra-exclude-words.txt --keep-sequence-bigrams --gram-sizes 1,2  --use-pipe-from "${d}"/classifier/"${c}".mallet   
+
+mallet infer-topics --input "${d}"/"${p}"/workshop/inferring-"${f}".mallet --inferencer "${d}"/classifier/"${c}"-inferencer.mallet --output-doc-topics "${d}"/"${p}"/output/scores-"${f}"-"${c}".txt   
+
 echo "Command sequence finished successfully"
