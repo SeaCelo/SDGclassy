cae7df46892167bce75edd04aeb5bcedd5666799
SeaCelo <SeaCelo@users.noreply.github.com>
2019-05-29 10:09:33 -0400

    fix paths

--- classifier/test_data/test_classifier.sh
+++ classifier/test_data/test_classifier.sh
@@ -3,16 +3,17 @@
 set -e     #needed to ignore errors below
 
 s='sdg14'                        					#testing data directory
-d='/Users/mlafleur/Projects/SDGclassy/classifier/test_data' 		#specify root path
+d='/Users/mlafleur/Projects/SDGclassy/classifier' 		#specify root path
+p='test_data'           #specify subdirectory for test
 cd "${d}" 
 
 for c in cl_base_new cl_base ;  do 
 
 rm "${d}"/"${s}"/.DS_Store || true   				#we ignore any errors here
 
-mallet import-dir --input "${d}"/"${s}"  --output "${d}"/workshop/test_"${s}"-"${c}".mallet --keep-sequence --remove-stopwords --extra-stopwords "${d}"/workshop/extra-exclude-words_alt.txt --keep-sequence-bigrams --gram-sizes 1,2  --use-pipe-from /Users/mlafleur/Projects/SDGclassy/classifier/results/"${c}".mallet   
+mallet import-dir --input "${d}"/"${p}"/"${s}"  --output "${d}"/"${p}"/workshop/test_"${s}"-"${c}".mallet --keep-sequence --remove-stopwords --extra-stopwords "${d}"/"${p}"/workshop/extra-exclude-words_alt.txt --keep-sequence-bigrams --gram-sizes 1,2  --use-pipe-from "${d}"/results/"${c}".mallet   
 
-mallet infer-topics --input "${d}"/workshop/test_"${s}"-"${c}".mallet --inferencer /Users/mlafleur/Projects/SDGclassy/classifier/results/"${c}"-inferencer.mallet --output-doc-topics "${d}"/output/scores_"${s}"-"${c}".txt   
+mallet infer-topics --input "${d}"/"${p}"/workshop/test_"${s}"-"${c}".mallet --inferencer "${d}"/results/"${c}"-inferencer.mallet --output-doc-topics "${d}"/"${p}"/output/scores_"${s}"-"${c}".txt   
 
 continue
 echo ""${c}" command sequence finished successfully"
