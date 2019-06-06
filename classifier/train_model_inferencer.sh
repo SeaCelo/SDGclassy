7b71c61438ae35e46b1987f3182a7396bb7849d8
SeaCelo <SeaCelo@users.noreply.github.com>
2019-06-06 15:13:10 -0400

    training cl_nounsadj

--- classifier/train_model_inferencer.sh
+++ classifier/train_model_inferencer.sh
@@ -3,7 +3,7 @@ set -e     #needed to ignore errors?
 
 d='/Users/mlafleur/Projects/SDGclassy'   #root path
 
-c='cl_base_new'                        #classifier training data subdirectory
+c='cl_nounsadj'                        #classifier training data subdirectory
 w='extra-exclude-words_new.txt'			#stop words file
 
 cd "${d}" && \
@@ -11,7 +11,7 @@ rm "${d}"/classifier/sources/"${c}"/.DS_Store || true   #we ignore any errors he
 
 mallet import-dir --input "${d}"/classifier/sources/"${c}"/  --output "${d}"/classifier/results/"${c}".mallet --keep-sequence --remove-stopwords --extra-stopwords "${d}"/workshop/"${w}" --keep-sequence-bigrams --gram-sizes 1,2 
 
-mallet train-topics --input "${d}"/classifier/results/"${c}".mallet --output-state "${d}"/classifier/state/"${c}"-state.gz --output-doc-topics "${d}"/classifier/topics/"${c}"-topics.txt  --output-topic-keys "${d}"/classifier/topics/"${c}"-keys.txt --output-topic-docs "${d}"/classifier/topics/"${c}"-top.txt  --optimize-interval 10 --num-topics 18 --output-model "${d}"/classifier/results/"${c}"-model 
+mallet train-topics --input "${d}"/classifier/results/"${c}".mallet --output-state "${d}"/classifier/state/"${c}"-state.gz --output-doc-topics "${d}"/classifier/topics/"${c}"-topics.txt  --output-topic-keys "${d}"/classifier/topics/"${c}"-keys.txt  --optimize-interval 10 --num-topics 18 --output-model "${d}"/classifier/results/"${c}"-model 
 
 mallet train-topics --input-model "${d}"/classifier/results/"${c}"-model --inferencer-filename "${d}"/classifier/results/"${c}"-inferencer.mallet --num-iterations 0  
 
