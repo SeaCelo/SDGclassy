d7e53e8b85c31e549a5cfc4152d0865f91414f7f
SeaCelo <SeaCelo@users.noreply.github.com>
2020-07-21 16:14:43 -0400

    just synching
    sync

--- project-pillars/classifier/train_model_inferencer.sh
+++ project-pillars/classifier/train_model_inferencer.sh
@@ -14,7 +14,7 @@ rm "${d}"/classifier/sources/"${c}"/.DS_Store || true   #Clean up on Mac. We ign
 mallet import-dir --input "${d}"/classifier/sources/"${c}"/  --output "${d}"/classifier/output/"${c}".mallet --keep-sequence --remove-stopwords --extra-stopwords "${d}"/"${w}" --keep-sequence-bigrams --gram-sizes 1,2 
 
 #Train topic model
-mallet train-topics --input "${d}"/classifier/output/"${c}".mallet --output-state "${d}"/classifier/state/"${c}"-state.gz --output-doc-topics "${d}"/classifier/topics/"${c}"-topics.txt  --output-topic-keys "${d}"/classifier/topics/"${c}"-keys.txt --num-top-words 30 --optimize-interval 10 --num-topics 9 --output-model "${d}"/classifier/output/"${c}"-model 
+mallet train-topics --input "${d}"/classifier/output/"${c}".mallet --output-state "${d}"/classifier/state/"${c}"-state.gz --output-doc-topics "${d}"/classifier/topics/"${c}"-topics.txt  --output-topic-keys "${d}"/classifier/topics/"${c}"-keys.txt --num-top-words 30 --optimize-interval 10 --num-topics 8 --output-model "${d}"/classifier/output/"${c}"-model 
 
 #Use model to create inferencer
 mallet train-topics --input-model "${d}"/classifier/output/"${c}"-model --inferencer-filename "${d}"/classifier/output/"${c}"-inferencer.mallet --num-iterations 0  
