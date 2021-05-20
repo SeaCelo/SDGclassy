f3453e7bc5a86438adfd69609976cca4b8fecf91
SeaCelo <SeaCelo@users.noreply.github.com>
2021-05-20 12:38:35 -0400

    update path to local mallet
    Change script to point to the installed Mallet as done in the ReadMe

--- infer-scores.sh
+++ infer-scores.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e          #needed to ignore errors?
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )  #we want to run Mallet from the current directory
 
 rm /target/input/.DS_Store || true   #we ignore any errors here
 
-mallet import-dir --input /target/input  --output /target/workshop/inferring-temp.mallet --keep-sequence --remove-stopwords --extra-stopwords extra-exclude-words_new.txt --keep-sequence-bigrams --gram-sizes 1,2  --use-pipe-from /classifier/sdg.mallet   
+"$parent_path"/mallet-2.0.8/bin/mallet import-dir --input /target/input  --output /target/workshop/inferring-temp.mallet --keep-sequence --remove-stopwords --extra-stopwords extra-exclude-words_new.txt --keep-sequence-bigrams --gram-sizes 1,2  --use-pipe-from /classifier/sdg.mallet   
 
-mallet infer-topics --input /target/workshop/inferring-temp.mallet --inferencer /classifier/sdg-inferencer.mallet --output-doc-topics /target/output/scores-out.txt   
+"$parent_path"/mallet-2.0.8/bin/mallet infer-topics --input /target/workshop/inferring-temp.mallet --inferencer /classifier/sdg-inferencer.mallet --output-doc-topics /target/output/scores-out.txt   
 
-echo "Command sequence finished successfully"
+echo "Command sequence finished successfully. The results are available in /target/output/scores-out.txt."
+echo "The mapping between the results and the SDGs is available in /classifier/topic-sdg_mapping.csv."
