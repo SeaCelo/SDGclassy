b96d5ede1c7aabeea06378b32280b0404db3f6f0
unknown <69007360+office-chair@users.noreply.github.com>
2021-05-26 19:54:17 -0400

    Hardcoded paths

--- infer-scores.ps1
+++ infer-scores.ps1
@@ -1,25 +1,16 @@
-[string]$d = Get-Location     # root path
-$c = 'sdg'                    # select classifier name
-$f = 'target'                 # select target files
-$w = 'extra-exclude-words.txt'
-
-Set-Location $env:MALLET_HOME
-
-bin/mallet import-dir `
-  --input $d/$f `
-  --output $d/$f/temp/inferring-$c.mallet `
+mallet-2.0.8/bin/mallet import-dir `
+  --input ./target `
+  --output ./target/temp/inferring-sdg.mallet `
   --keep-sequence `
   --remove-stopwords `
-  --extra-stopwords $d/classifier/$w `
+  --extra-stopwords ./classifier/extra-exclude-words.txt `
   --keep-sequence-bigrams `
-  --use-pipe-from $d/classifier/$c.mallet
+  --use-pipe-from ./classifier/sdg.mallet
 
-bin/mallet infer-topics `
-  --input $d/$f/temp/inferring-$c.mallet `
-  --inferencer $d/classifier/$c-inferencer.mallet `
-  --output-doc-topics $d/$f/output/scores-$c.txt
+mallet-2.0.8/bin/mallet infer-topics `
+  --input ./target/temp/inferring-sdg.mallet `
+  --inferencer ./classifier/sdg-inferencer.mallet `
+  --output-doc-topics ./target/output/scores-out.txt
 
-Write-Output "Command sequence finished successfully. The results are available in /target/output/scores-$c.txt."
+Write-Output "Command sequence finished successfully. The results are available in /target/output/scores-out.txt."
 Write-Output "The mapping between the results and the SDGs is available in /classifier/topic-sdg_mapping.csv."
-
-Pause
