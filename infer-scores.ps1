3a598f5c7ef66e48b366868cf5980697c19fe366
SeaCelo <SeaCelo@users.noreply.github.com>
2024-02-02 14:11:20 -0500

    New model and instructions
    Updated with new model that includes LLM training data. The previous model is now "_old". Renamed files. Updated ReadMe. Missing large files: "inferring-temp.mallet" and "sdgclassy.mallet". They are too big and need to be added using Git LFS

--- infer-scores.ps1
+++ infer-scores.ps1
@@ -1,16 +1,16 @@
 mallet-2.0.8/bin/mallet import-dir `
   --input ./target `
-  --output ./target/temp/inferring-sdg.mallet `
+  --output ./target/temp/inferring-temp.mallet `
   --keep-sequence `
   --remove-stopwords `
   --extra-stopwords ./classifier/extra-exclude-words.txt `
   --keep-sequence-bigrams `
-  --use-pipe-from ./classifier/sdg.mallet
+  --use-pipe-from ./classifier/sdgclassy.mallet
 
 mallet-2.0.8/bin/mallet infer-topics `
-  --input ./target/temp/inferring-sdg.mallet `
-  --inferencer ./classifier/sdg-inferencer.mallet `
-  --output-doc-topics ./target/output/scores-out.txt
+  --input ./target/temp/inferring-temp.mallet `
+  --inferencer ./classifier/sdgclassy-inferencer.mallet `
+  --output-doc-topics ./target/output/SDG-scores-out.txt
 
-Write-Output "Command sequence finished successfully. The results are available in /target/output/scores-out.txt."
+Write-Output "Command sequence finished successfully. The results are available in /target/output/SDG-scores-out.txt."
 Write-Output "The mapping between the results and the SDGs is available in /classifier/topic-sdg_mapping.csv."
--- infer-scores2.sh
+++ infer-scores2.sh
@@ -35,12 +35,13 @@ set -e          #needed to ignore errors?
 
 rm "${INPUT}"/.DS_Store || true   #we ignore any errors here
 
-./mallet-2.0.8/bin/mallet import-dir --input "${INPUT}"  --output ./classifier/inferring-temp.mallet --keep-sequence --remove-stopwords --extra-stopwords ./classifier/extra-exclude-words.txt --keep-sequence-bigrams --gram-sizes 1,2  --use-pipe-from ./classifier/sdg.mallet   
+./mallet-2.0.8/bin/mallet import-dir --input "${INPUT}"  --output ./classifier/inferring-temp.mallet --keep-sequence --remove-stopwords --extra-stopwords ./classifier/extra-exclude-words.txt --keep-sequence-bigrams --gram-sizes 1,2  --use-pipe-from ./classifier/sdgclassy.mallet   
 
 
-./mallet-2.0.8/bin/mallet infer-topics --input ./classifier/inferring-temp.mallet --inferencer ./classifier/sdg-inferencer.mallet --output-doc-topics "${OUTPUT}"/SDG-scores.txt   
+./mallet-2.0.8/bin/mallet infer-topics --input ./classifier/inferring-temp.mallet --inferencer ./classifier/sdgclassy-inferencer.mallet --output-doc-topics "${OUTPUT}"/SDG-scores-out.txt   
 
-echo "Command sequence finished successfully. The results are available in the file SDG-scores.txt."
+echo "Command sequence finished successfully using the sdgclassy.mallet model." 
+echo "The results are available in the file SDG-scores-out.txt."
 echo "The mapping between the results and the SDGs is available in /classifier/topic-sdg_mapping.csv."
 
 exit 0
